fc93e9ea250fdded49f9f9cca059b77e03a61b34
hfbsdcausa <hbruyns@nextivityinc.com>
2015-12-02 12:34:32 -0800

    dbg

--- www/js/msg_nxty.js
+++ www/js/msg_nxty.js
@@ -1422,9 +1422,11 @@ var nxty = {
                 // Rx  ae xx yy 55   50 0  0 0 0 ....  crc
                 //     [0]           [4]        
                 var payloadLen = u8RxBuff[1];
-                PrintLog(1, "WF 1a " + uCmd + "," + u8RxBuff[1] + "," + u8RxBuff.length);
-                var datapayload = u8RxBuff.slice(4, 4+u8RxBuff[1]);  //zero based index for start and end of data
-                PrintLog(1, "WF 1b " + datapayload.length + "," + payloadLen);
+                PrintLog(1, "WF 1a " + uCmd + "," + u8RxBuff[1] + "," + u8RxBuff.length+ "," + payloadLen);
+                if(u8RxBuff.slice === undefined) {PrintLog(1, "u8RxBuff.slice undefined" );}
+                PrintLog(1, "u8RxBuff.slice" + u8RxBuff.slice);
+                var datapayload = u8RxBuff.slice(4, 4+payloadLen);  //zero based index for start and end of data
+                PrintLog(1, "WF 1b " + datapayload.length );
                 writeNlogFile(datapayload);
                 PrintLog(1, "WF 2");
                 break;
